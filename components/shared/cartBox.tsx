7b4b7a326482b0aa0414ab694486b7def903989e
Vova Dovhal <kadaekadae@gmail.com>
2025-01-29 17:08:47 +0200

    add react-use intesection

--- components/shared/cartBox.tsx
+++ components/shared/cartBox.tsx
@@ -1,12 +1,53 @@
+"use client"
+import { useEffect, useRef, useState } from 'react';
 import './cartBox.css'
 import CartGroup from './cartGroup';
+import { useIntersection } from 'react-use';
+
+const titles =
+    [
+        {
+            id: 1,
+            name: "Одяг",
+        },
+        {
+            id: 2,
+            name: "Аксесуари",
+        },
+        {
+            id: 3,
+            name: "Диски",
+        },
+        {
+            id: 4,
+            name: "Касети",
+        },
+        {
+            id: 5,
+            name: "Платівки",
+        }
+    ]
 
 const CartBox: React.FC = () => {
-    const titles = ["Одяг", "Аксесуари", "Диски", "Касети", "Платівки"]
+    const [idCat, setIdCat] = useState(1)
+    const intersectionRef: any = useRef(null);
+
+    const intersection = useIntersection(intersectionRef, {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1
+    });
+
+    useEffect(() => {
+        if (intersection?.isIntersecting) {
+            console.log(idCat)
+        }
+    }, [intersection?.isIntersecting])
+
     return (
         <div className="cart-box-container">
-            {titles.map((el, index) =>
-                <CartGroup key={index} title={el} />
+            {titles.map((el) =>
+                <CartGroup key={el.id} title={el.name} idCat={el.id} intersectionRef={intersectionRef} />
             )}
         </div>
     )
--- components/shared/cartGroup.tsx
+++ components/shared/cartGroup.tsx
@@ -3,11 +3,13 @@ import Cart from "./cart";
 
 interface CartGroupTypeProps {
     title: string;
+    idCat: number;
+    intersectionRef: any;
 }
 
-const CartGroup: React.FC<CartGroupTypeProps> = ({ title }) => {
+const CartGroup: React.FC<CartGroupTypeProps> = ({ title, idCat, intersectionRef }) => {
     return (
-        <div className="cart-group">
+        <div className="cart-group" id={title} ref={intersectionRef}>
             <h2 className='cart-group-title'>{title}</h2>
             <div className='cart-group-list'>
                 <Cart />
